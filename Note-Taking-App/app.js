8620ac0925bd53d0ef543ed6152d15711076b6c9
faridahokeniyi <122537816+faridahokeniyi@users.noreply.github.com>
2025-12-19 16:46:58 +0100

    notetaking app tut (on hold)

---
+++ Note-Taking-App/app.js
@@ -0,0 +1,8 @@
+const noteContainer = document.querySelector(".note-container")
+const createBtn = document.querySelector(".btn")
+let notes = document.querySelectorAll(".input-box")
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("textarea")
+  let
+})
